cd90184a2aabe713887300d12ee23427c0b0afb5
Franz Wollang <franzwollang@gmail.com>
2022-06-29 11:40:20 +0200

    PoC of flip cards on grid

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 const cardGrid = document.getElementById("card-grid") as HTMLElement;
 
+function addClasses(element: HTMLElement, classes: Array<string>) {
+	element.classList.add(...classes);
+}
+
+function newElement(tag: string, classes: Array<string> = []) {
+	const element = document.createElement(tag);
+	addClasses(element, classes);
+	return element;
+}
+
 function addChildren(parent: HTMLElement, children: Array<HTMLElement>): void {
 	for (const child of children) {
 		parent.appendChild(child);
@@ -9,13 +19,32 @@ function addChildren(parent: HTMLElement, children: Array<HTMLElement>): void {
 function placeCards(cards: Array<any>): void {
 	// transform cards data into appropriate elements
 	const cardElements = cards.map((card) => {
-		const element = document.createElement("div");
-		element.innerText = card.frontText;
-		element.classList.add("bg-blue-400");
+		const element = newElement("div", ["grid-cell", "bg-blue-400"]);
+		addChildren(element, [card]);
 		return element;
 	});
 
 	addChildren(cardGrid, cardElements);
+}
+
+function FlipBox(
+	frontChildren: Array<HTMLElement>,
+	backChildren: Array<HTMLElement>
+) {
+	const flipBox = newElement("div", ["flip-box"]);
+
+	const flipBoxInner = newElement("div", ["flip-box-inner"]);
+	addChildren(flipBox, [flipBoxInner]);
+
+	const flipBoxFront = newElement("div", ["flip-box-front"]);
+	addChildren(flipBoxFront, frontChildren);
+
+	const flipBoxBack = newElement("div", ["flip-box-back"]);
+	addChildren(flipBoxBack, backChildren);
+
+	addChildren(flipBoxInner, [flipBoxFront, flipBoxBack]);
+
+	return flipBox;
 }
 
 const testCards = Array.from({ length: 18 }, () => {
@@ -25,4 +54,14 @@ const testCards = Array.from({ length: 18 }, () => {
 	};
 });
 
-placeCards(testCards);
+const flipCards = testCards.map((card) => {
+	const front = newElement("span");
+	front.innerText = card.frontText;
+
+	const back = newElement("span");
+	back.innerText = card.backText;
+
+	return FlipBox([front], [back]);
+});
+
+placeCards(flipCards);
